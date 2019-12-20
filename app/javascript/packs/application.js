9f7c1b9b55b79626983f114066ac7513b808f514
paul lahana <paul.lahana@gmail.com>
2019-12-20 17:17:32 +0100

    console hi

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,5 @@
-console.log('Hello World from Webpacker')
+console.log("Hi there ! I'm paultursuru and I made this with rails :)")
+console.log("You can find me on github <3")
 
 require("@rails/actiontext")
 require("trix")
