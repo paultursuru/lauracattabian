d1b57a9685e4f805ddc125d902bc803ea2089ea0
paul lahana <paul.lahana@gmail.com>
2019-12-20 10:04:30 +0100

    params for mailer before adding mailform

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,22 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "lauracattabian_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { :host => 'lauracatta.herokuapp.com' }
+  # Rails.application.routes.default_url_options[:host] = 'lauracatta.herokuapp.com'
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: ENV["GMAIL_DOMAIN"],
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
   # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Ignore bad email addresses and do not raise email delivery errors.
