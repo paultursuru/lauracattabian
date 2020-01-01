33be70b3e0a73635cd3bf2f460154d75e6948531
paul lahana <paul.lahana@gmail.com>
2020-01-01 17:08:03 +0100

    new email client

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
+    address: "smtp.yahoo.com",
     port: 587,
     domain: ENV["GMAIL_DOMAIN"],
     authentication: "plain",
